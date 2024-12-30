2aa197cb586e416a8888cd59575d43c37ce6f0d4
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-12-31 00:39:30 +0600

    get data from the create assignment form

--- src/Pages/CreateAssignment.jsx
+++ src/Pages/CreateAssignment.jsx
@@ -4,14 +4,27 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const CreateAssignment = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [selected, setSelected] = useState("select");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const title = form.get("title");
+    const phoroUrl = form.get("phoroUrl");
+    const marks = form.get("marks");
+    const description = form.get("description");
+    console.log(title, phoroUrl, marks, description, selected, startDate);
+    // e.target.reset();
+  };
+
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero min-h-screen my-12 container mx-auto">
       <div className="hero-content flex-col">
         <div className="text-center">
           <h1 className="text-5xl font-bold">Create Assignment</h1>
         </div>
-        <div className="card w-full max-w-lg shrink-0 shadow-2xl">
-          <form className="card-body">
+        <div className="card shrink-0 shadow-2xl w-full">
+          <form onSubmit={handleForm} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Title</span>
@@ -36,37 +49,59 @@ const CreateAssignment = () => {
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Total Marks</span>
-              </label>
-              <input
-                type="number"
-                name="marks"
-                placeholder="Enter assignment total marks"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Difficulty Level</span>
-              </label>
-              <select name="" id="">
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-              </select>
+
+            <div className="flex gap-4">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Total Marks</span>
+                </label>
+                <input
+                  type="number"
+                  name="marks"
+                  placeholder="Assignment total marks"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Difficulty Level</span>
+                </label>
+                <select
+                  value={selected}
+                  onChange={(e) => setSelected(e.target.value)}
+                  className="border-2 p-3 rounded-lg"
+                  name=""
+                  id=""
+                >
+                  <option value="easy">Easy</option>
+                  <option value="medium">Medium</option>
+                  <option value="hard">Hard</option>
+                </select>
+              </div>
             </div>
             <div>
               <label className="label">
                 <span className="label-text">Due to</span>
               </label>
-              <div>
+              <div className="w-full">
                 <DatePicker
+                  className="border-2 p-3 rounded-lg"
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
                 ></DatePicker>
+              </div>
+            </div>
+            <div>
+              <label className="label">
+                <span className="lebel-text">Description</span>
+              </label>
+              <div className="w-full">
+                <textarea
+                  className="textarea textarea-bordered w-full"
+                  name="description"
+                  placeholder="Description here"
+                ></textarea>
               </div>
             </div>
             <div className="form-control mt-6">
